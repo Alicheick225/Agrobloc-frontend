f7b2c3e913255fe85793e4b8b16ae4a0f7ab536d
Rokia98 <crokia042@gmail.com>
2025-06-13 18:02:46 +0000

    ssh probleme

--- src/app/annonces/annonce-financement/annonce-financement.ts
+++ src/app/annonces/annonce-financement/annonce-financement.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+   standalone: true,
   selector: 'app-annonce-financement',
   imports: [],
   templateUrl: './annonce-financement.html',
